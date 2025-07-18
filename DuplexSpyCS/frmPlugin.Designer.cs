83c38322b66ad92b0ce0e97a41493dc6e24ee2b2
malbuffer4pt <icbluewhale@gmail.com>
2025-07-18 17:28:56 +0800

    update

---
+++ DuplexSpyCS/frmPlugin.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DuplexSpyCS
+{
+    partial class frmPlugin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            toolStrip1 = new ToolStrip();
+            statusStrip1 = new StatusStrip();
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(909, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 429);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(909, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // listView1
+            // 
+            listView1.Dock = DockStyle.Fill;
+            listView1.Location = new Point(0, 25);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(909, 404);
+            listView1.TabIndex = 2;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // frmPlugin
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(909, 451);
+            Controls.Add(listView1);
+            Controls.Add(statusStrip1);
+            Controls.Add(toolStrip1);
+            Font = new Font("Microsoft JhengHei UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "frmPlugin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmPlugin";
+            Load += frmPlugin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private StatusStrip statusStrip1;
+        private ListView listView1;
+    }
+}
